ce3a3413f7b6a49525cf154f7abe422a33f5a8a0
Rilson Joás <107469259+narniano@users.noreply.github.com>
2023-07-15 17:58:36 -0300

    Todo List updated

--- app.js
+++ app.js
@@ -2,30 +2,43 @@
 
 const express = require("express");
 const bodyParser = require("body-parser");
+const date = require(__dirname + "/date.js")
 
 const app = express();
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-let novoItem = []; //Essa variável de escopo global está aqui porque vai ser usada tanto no app.get quanto no app.post
+const novosItens = []; //Essa variável de escopo global está aqui porque vai ser usada tanto no app.get quanto no app.post
+const itensDoTrabalho = [];
 
 app.get("/", (req, res) => {
-  let options = {
-    weekday: "long",
-    day: "numeric",
-    month: "long",
-  };
-  let today = new Date();
-  let day = today.toLocaleDateString("pt-BR", options);
 
-  res.render("list", { qualDia: day, novoItem: novoItem }); //O res.render carrega como página na rota "/" o "list.ejs". O res.render precisa conter todos os <%= %> da página de uma vez no app.get. Para que quando a página renderize, ela já saiba o que fazer.
+  const day = date.getDate();
+  res.render("list", { listTitle: day, novoItem: novosItens }); //O res.render carrega como página na rota "/" o "list.ejs". O res.render precisa conter todos os <%= %> da página de uma vez no app.get. Para que quando a página renderize, ela já saiba o que fazer.
 });
 
+app.get("/work", function (req, res) {
+  res.render("list", {
+    listTitle: "Trabalho",
+    novoItem: itensDoTrabalho,
+  });
+});
+
+app.get("/sobre", function(req, res) {
+  res.render("sobre");
+})
+
 app.post("/", (req, res) => {
-  let item = req.body.adicionar;
-  novoItem.push(item); //Inclui o valor submetido pelo botão no array de escopo global.
-  res.redirect("/"); //Faz com que o conteúdo do app.post seja enviado de volta ao site com valores atualizados, e levando em conta o res.render.
+  const item = req.body.adicionar;
+
+  if (req.body.item === "Trabalho") {
+    itensDoTrabalho.push(item);
+    res.redirect("/work"); //Faz com que o conteúdo do app.post seja enviado de volta ao site com valores atualizados, e levando em conta o res.render.
+  } else {
+    novosItens.push(item); //Inclui o valor submetido pelo botão no array de escopo global.
+    res.redirect("/"); //Faz com que o conteúdo do app.post seja enviado de volta ao site com valores atualizados, e levando em conta o res.render.
+  }
 });
 
 app.listen(3000, function () {
